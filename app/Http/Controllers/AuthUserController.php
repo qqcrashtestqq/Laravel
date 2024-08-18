6e9e095a85fa222818c9d91b3026379a511755b5
qqcrashtestqq <artemlcenko@gmail.com>
2024-08-18 13:39:58 +0300

    first commit

---
+++ app/Http/Controllers/AuthUserController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AuthUserController extends Controller
+{
+    public function authUser() {
+        return 'this page auth user';
+    }
+}
