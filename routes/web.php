9d14f2a2c6c74fc85c5cf68dec6d39b326a1b98d
qqcrashtestqq <artemlcenko@gmail.com>
2024-08-18 14:39:03 +0300

    clear project , delete test files

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,3 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function() {
-    // dump(config('app.locale'));
-    // dump(config('database.connections.mysql'));
-    dump(config('costom.costom', 'test'));
-});
