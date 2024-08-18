600d23783a2fad61b94cb41e6abe53cd510aedfc
qqcrashtestqq <artemlcenko@gmail.com>
2024-08-18 14:15:21 +0300

    create new test file for route web

---
+++ config/constom.php
@@ -0,0 +1,5 @@
+<?
+
+return [
+    'costom' => 'My test var',
+];
--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function() {
     // dump(config('app.locale'));
-    dump(config('database.connections.mysql'));
+    // dump(config('database.connections.mysql'));
+    dump(config('costom.costom', 'test'));
 });
