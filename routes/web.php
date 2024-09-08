95b2b85a8dd5f0db450e6c3757b0841d510f6fee
qqcrashtestqq <artemlcenko@gmail.com>
2024-09-08 11:55:31 +0300

    create register user

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use App\Http\Controllers\TestController;
-use App\Http\Controllers\UserController;
+
+use App\Http\Controllers\RegisterUserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,7 +15,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::resource('test', TestController::class);
+
 
 //resource routs user (create, update , delete )
-Route::resource('user', UserController::class);
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::resource('users', RegisterUserController::class);
